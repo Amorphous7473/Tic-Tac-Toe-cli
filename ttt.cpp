2bf10824c51a129fd768d6b8e1594f8b0e665b70
Amorphous <shrey7473@protonmail.com>
2026-09-10 16:12:55 +0530

    Update ttt.cpp

--- ttt.cpp
+++ ttt.cpp
@@ -14,8 +14,8 @@ bool win(int t[3][3])
     }
     if(t[1][1] == t[2][2] && t[2][2] == t[0][0] && t[2][2] == 2)return true;
     if(t[1][1] == t[2][2] && t[2][2] == t[0][0] && t[2][2] == 3)return true;
-    if(t[0][2] == t[2][2] && t[1][1] == t[2][0] && t[1][1] == 2)return true;
-    if(t[0][2] == t[2][2] && t[1][1] == t[2][0] && t[1][1] == 3)return true;
+    if(t[0][2] == t[1][1] && t[1][1] == t[2][0] && t[1][1] == 2)return true;
+    if(t[0][2] == t[1][1] && t[1][1] == t[2][0] && t[1][1] == 3)return true;
     return false;
 }
 //Check if the element is empty or not
